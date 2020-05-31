5182acb9c58106321a9320e29614ff85037aa69d
Alexandre Mendonça Álvaro <alexandre@MacBook-Pro-de-Alexandre.local>
2020-05-31 01:41:28 -0300

    fix Dlib install reference

--- easy-install-after-reboot.sh
+++ easy-install-after-reboot.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
+echo "Preparing OpenCV Install.."
 cd ~/install-opencv-dlib-raspbian
 ~/install-opencv-dlib-raspbian/install.sh
 
 PROJECT_PATH=$(<~/install-opencv-dlib-raspbian/current-dir.txt)
 
 cd $PROJECT_PATH
-pipenv install && pipenv shell
+pipenv install
+pipenv shell
 
 echo "Linking OpenCV to Project Virtual Enviroment..."
 ~/install-opencv-dlib-raspbian/link-virtualenv.sh
+
+echo "Starting Dlib Install.."
+pipenv install 'numpy==1.18.4' 'dlib==19.19.0'
 
 echo "Returning system parameters..."
 ~/install-opencv-dlib-raspbian/swapfile.sh 100
